128157c967ac263681fdba692dd6e1348b50a3e2
root <root@localhost.localdomain>
2018-11-19 11:02:12 +0300

    second change

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "ok ! my first change"
+    return 'ok ! my first change'
 
 if __name__ == "__main__":
     app.run() 
